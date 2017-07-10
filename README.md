8d7b9ae0e5f09b6357c8a10582a2352a5a4f4b0b
M Rule <mrule7404@gmail.com>
2017-07-10 16:41:06 +0100

    update to a more formal readme

---
+++ README.md
@@ -0,0 +1,49 @@
+# CGID libraries
+
+Utility routines for interacting with the CGID datasets. These are
+"cued grasping with instructed delay" task recordings from multi-
+electrode arrays in primate motor cortex. 
+
+These routines depend on [miscellaneous python routines for 
+computational neuroscience](
+http://michaelerule.github.io/neurotools/_build/html/index.html)
+
+Rudimentary documentation is available on [github pages](
+http://michaelerule.github.io/cgid/_build/html/index.html)
+
+This read-me original contained the following grave disclaimer:
+
+>   This is not a library, module or package. It should not be used
+>   by anyone, for any purpose, ever. This entire code-base should be
+>   broken, all of the time, and this is intentional. This is 
+>   internal code and depends on files not available in the 
+>   repository.
+
+In reality, this repository is simply not-maintained and perpetually
+"under construction". The hope is mainly that (1) if any code 
+contained herein is ever found to be useful to anyone,
+then it can be re-purposed and (2) should any future students work 
+with multi-electrode array datasets, then this project provides and 
+archive of past research code to facilitate reproducibility and 
+future exploratory research. 
+
+In the event that work concludes on said datasets, and if this 
+project is able to obtain the data under a suitable license, then
+the CGID datasets will be incorporated into this repository for
+archival and reproducibility.
+
+This project is published in the spirit (but not the letter) of the 
+[tongue-and-cheek CRAPL license for academic code](
+http://matt.might.net/articles/crapl/CRAPL-LICENSE.txt). This is not 
+a legal license as it contains internal inconsistencies (like 
+forbidding users to exist). However, it contains a strong caution 
+that this project has not been reviewed or prepared for use by the 
+general public. For now, this project and associated content is 
+licensed under the [Creative Commons Attribution Non Commercial 
+Share Alike 3.0 license](
+https://creativecommons.org/licenses/by-nc-sa/3.0/) 
+[(full license)](
+https://creativecommons.org/licenses/by-nc-sa/3.0/legalcode). 
+Please open an issue if the license structure appears to forbid 
+re-use, the license itself is also "under construction".
+
--- phase_plots.py
+++ phase_plots.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-# BEGIN PYTHON 2/3 COMPATIBILITY BOILERPLATE
 from __future__ import absolute_import
 from __future__ import with_statement
 from __future__ import division
@@ -8,21 +7,22 @@ from __future__ import nested_scopes
 from __future__ import generators
 from __future__ import unicode_literals
 from __future__ import print_function
-import sys
-# more py2/3 compat
 from neurotools.system import *
-if sys.version_info<(3,):
-    from itertools import imap as map
-# END PYTHON 2/3 COMPATIBILITY BOILERPLATE
 
 '''
 Utility functions for making nicely upsampled plots of phase delays and
 phase gradients for the 10x10 Utah arrays.
 '''
 
-from neurotools.color import *
-from matplotlib.pyplot import *
-from neurotools.nlab import *
+# TODO: replace with specific absolute imports
+# from neurotools.color import *
+# from matplotlib.pyplot import *
+# from neurotools.nlab import *
+
+from neurotools.color import parula,extended,isolum
+from neurotools.plot import nox,noy,fudgex,fudgey,noaxis
+import matplotlib.pyplot as plt
+import numpy as np
 
 def array_imshow(data,vmin=None,vmax=None,cmap=extended,origin='lower',
     drawlines=1,interpolation='bicubic',extent=(0,4,0,4),ctitle='',spacing=0,
@@ -39,16 +39,16 @@ def array_imshow(data,vmin=None,vmax=None,cmap=extended,origin='lower',
         interpolation=interpolation,extent=extent)
     if drawlines:
         for i in linspace(0,10,11):
-            axvline(i*4.0/10,color='w',lw=0.3)
-            axhline(i*4.0/10,color='w',lw=0.3)
-    xlim(0,4)
-    ylim(0,4)
-    nicexy()
-    xlabel('mm')
-    ylabel('mm')
-    fudgex()
-    fudgey(3)
-    draw()
+            plt.axvline(i*4.0/10,color='w',lw=0.3)
+            plt.axhline(i*4.0/10,color='w',lw=0.3)
+    plt.xlim(0,4)
+    plt.ylim(0,4)
+    plt.nicexy()
+    plt.xlabel('mm')
+    plt.ylabel('mm')
+    plt.fudgex()
+    plt.fudgey(3)
+    plt.draw()
     if draw_colorbar:
         cax=good_colorbar(vmin,vmax,cmap,ctitle,
             sideways=1,spacing=spacing)
@@ -74,8 +74,8 @@ def array_plot_upsampled(data,factor,vmin=None,vmax=None,
     upsampled = dct_upsample(data,factor)
     # upsampling trims the array a bit
     # figure out how to re-center the trimmed array:
-    sw,sh  = shape(upsampled)
-    fw,fh  = float32(shape(data)[:2])*factor
+    sw,sh  = np.shape(upsampled)
+    fw,fh  = np.float32(np.shape(data)[:2])*factor
     dw,dh  = (fw-sw)/fw*0.5*4,(fh-sh)/fw*0.5*4
     extent = (dw,dw+sw/fw*4,dh,dh+sh/fh*4)
     print('extent=',extent)
@@ -91,17 +91,17 @@ def array_plot_upsampled(data,factor,vmin=None,vmax=None,
     #crossbad(M)
     if drawlines:
         for i in linspace(0,10,11):
-            axvline(i*4.0/10,color='w',lw=0.3)
-            axhline(i*4.0/10,color='w',lw=0.3)
-    xlim(0,4)
-    ylim(0,4)
-    nicex()
-    nicey()
-    xlabel('mm')
-    ylabel('mm')
-    fudgex()
-    fudgey(3)
-    draw()
+            plt.axvline(i*4.0/10,color='w',lw=0.3)
+            plt.axhline(i*4.0/10,color='w',lw=0.3)
+    plt.xlim(0,4)
+    plt.ylim(0,4)
+    plt.nicex()
+    plt.nicey()
+    plt.xlabel('mm')
+    plt.ylabel('mm')
+    plt.fudgex()
+    plt.fudgey(3)
+    plt.draw()
     cax=good_colorbar(vmin,vmax,cmap,ctitle,sideways=1)
     fudgey(4,cax)
     return cax
@@ -116,19 +116,19 @@ def overlay_gradient(phase_gradient):
             a = dz*.7*exp( .5j);
             b = dz*.7*exp(-.5j);
             px = p[col]
-            plot([px,px+real(dz)],[py,py+imag(dz)],color='k')[0]
-            plot([px,px-real(dz)],[py,py-imag(dz)],color='k')[0]
-            plot([px-a.real,px-real(dz),px-b.real],
+            plt.plot([px,px+real(dz)],[py,py+imag(dz)],color='k')[0]
+            plt.plot([px,px-real(dz)],[py,py-imag(dz)],color='k')[0]
+            plt.plot([px-a.real,px-real(dz),px-b.real],
                  [py-a.imag,py-imag(dz),py-b.imag],color='k')[0]
-    gca().tick_params(axis=u'both', which=u'both',length=0)
+    plt.gca().tick_params(axis=u'both', which=u'both',length=0)
 
 def get_upsampled_extent(upsampled_shape):
     assert 0 # not implemented
     warn('assuming 10x10?')
     # upsampling trims the array a bit
     # figure out how to re-center the trimmed array:
-    sw,sh  = shape(upsampled)
-    fw,fh  = float32(shape(mean_analytic_signal)[:2])*UPSAMPLE
+    sw,sh  = np.shape(upsampled)
+    fw,fh  = np.float32(np.shape(mean_analytic_signal)[:2])*UPSAMPLE
     dw,dh  = (fw-sw)/fw*0.5*4,(fh-sh)/fw*0.5*4
     extent = (dw,dw+sw/fw*4,dh,dh+sh/fh*4)
 
@@ -139,7 +139,7 @@ def phase_delay_plot(mean_analytic_signal,
     Accepts an analytic signal map, upsamples it, and plots in the current
     axis the phases. For now, expects a 10x10 array 4x4mm is size.
     '''
-    cla()
+    plt.cla()
     upsampled = dct_upsample(dct_cut_antialias(mean_analytic_signal,smooth),UPSAMPLE)
     # upsampling trims the array a bit
     # figure out how to re-center the trimmed array:
@@ -149,10 +149,10 @@ def phase_delay_plot(mean_analytic_signal,
     extent = (dw,dw+sw/fw*4,dh,dh+sh/fh*4)
     # extract angles
     if recenter:
-        amean  = angle(mean(upsampled))
-        angles = (angle(upsampled*exp(-1j*amean))+2*pi+pi+1.75)%(2*pi)
+        amean  = np.angle(np.mean(upsampled))
+        angles = (np.angle(upsampled*np.exp(-1j*amean))+2*pi+pi+1.75)%(2*pi)
     else:
-        angles = (angle(upsampled)+2*pi+pi+1.75)%(2*pi)
+        angles = (np.angle(upsampled)+2*pi+pi+1.75)%(2*pi)
     # plot phase angles
     cax = array_imshow(angles,0,2*pi,
         cmap=cm,
@@ -161,20 +161,20 @@ def phase_delay_plot(mean_analytic_signal,
         draw_colorbar=draw_colorbar)
     # add countours
     for phi in linspace(0,pi,NLINE+1)[:-1]:
-        c=contour((angles+phi)%(2*pi),[pi],linewidths=0.8,colors='k',extent=extent)
+        c=plt.contour((angles+phi)%(2*pi),[pi],linewidths=0.8,colors='k',extent=extent)
     # Add title
     title('Average phase delay',fontsize=13)
     # fix up colorbar axis labels
-    oldax = gca()
+    oldax = plt.gca()
     if draw_colorbar:
-        sca(cax)
-        noaxis()
+        plt.sca(cax)
+        plt.noaxis()
         cax.tick_params(axis=u'both', which=u'both',length=0,labelleft='off', labelright='on')
-        yticks([0,2*pi],['0','$2\pi$'])
+        plt.yticks([0,2*pi],['0','$2\pi$'])
         # redraw the plot
-        sca(oldax)
-    xlabel('mm',fontsize=11)
-    draw()
+        plt.sca(oldax)
+    plt.xlabel('mm',fontsize=11)
+    plt.draw()
     return cax,extent
 
 
@@ -185,43 +185,43 @@ def vector_summary_plot_subroutine(mu,sigma,vectors):
 
     TODO: circular gaussian plot is a little off here
     '''
-    cla()
+    plt.cla()
     limit = 2.0
 
     # draw the vectors
     for v in vectors:
-        arrow(0,0,v.real,v.imag,head_width=0.05,head_length=0.1,fc='k',color='k',lw=.2)
+        plt.arrow(0,0,v.real,v.imag,head_width=0.05,head_length=0.1,fc='k',color='k',lw=.2)
 
     # draw some axis lines
-    plot([-limit,limit],[0,0],lw=2,color='k')
-    plot([0,0],[-limit,limit],lw=2,color='k')
-    xlim(-limit,limit)
-    ylim(-limit,limit)
+    plt.plot([-limit,limit],[0,0],lw=2,color='k')
+    plt.plot([0,0],[-limit,limit],lw=2,color='k')
+    plt.xlim(-limit,limit)
+    plt.ylim(-limit,limit)
 
     # plot the phase distribution as if circular gaussian (?correct?)
-    h = linspace(-pi,pi,361)
+    h = np.linspace(-pi,pi,361)
     p = 1.2
-    plot(cos(h+mu)*p,sin(h+mu)*p,lw=0.5,color='k')
+    plt.plot(np.cos(h+mu)*p,np.sin(h+mu)*p,lw=0.5,color='k')
     p = npdf(0,sigma,h)*0.4 + 1.2
-    plot(cos(h+mu)*p,sin(h+mu)*p,lw=0.5,color='k')
+    plt.plot(np.cos(h+mu)*p,np.sin(h+mu)*p,lw=0.5,color='k')
 
     # clean up the plot
     force_aspect(1)
-    draw()
+    plt.draw()
     noaxis()
     nox()
     noy()
 
     # draw a mean vector
-    mv = mean(vectors)
-    arrow(0,0,mv.real,mv.imag,head_width=0.05,head_length=0.1,
+    mv = np.mean(vectors)
+    plt.arrow(0,0,mv.real,mv.imag,head_width=0.05,head_length=0.1,
         fc='w',color='w',lw=5,zorder=1e9)
-    arrow(0,0,mv.real,mv.imag,head_width=0.05,head_length=0.1,
+    plt.arrow(0,0,mv.real,mv.imag,head_width=0.05,head_length=0.1,
         fc='r',color='r',lw=3,zorder=1e10)
 
     # draw annotation for circular standard deviation
-    #plot([1.2,1.2],[0,sigma],lw=1,color='k')
-    #text(1.2+pixels_to_xunits(3),sigma/2,'$S$',
+    #plt.plot([1.2,1.2],[0,sigma],lw=1,color='k')
+    #plt.text(1.2+pixels_to_xunits(3),sigma/2,'$S$',
     #    horizontalalignment='left',
     #    verticalalignment='center',
     #    fontsize=16)
@@ -231,11 +231,11 @@ def vector_summary_plot(vectors):
     Does a summary plot of the phases of the vectors provided.
     vectors should be an array of complex numners
     '''
-    cla()
-    vectors = vectors/abs(vectors)
-    R  = abs(mean(vectors))
-    S  = sqrt(-2*log(R))
-    mu = angle(mean(vectors))
+    plt.cla()
+    vectors = vectors/np.abs(vectors)
+    R  = np.abs(np.mean(vectors))
+    S  = np.sqrt(-2*np.log(R))
+    mu = np.angle(np.mean(vectors))
     vector_summary_plot_subroutine(mu,S,vectors)
 
 def weighted_vector_summary_plot(vectors):
@@ -246,11 +246,11 @@ def weighted_vector_summary_plot(vectors):
     CAUTION: Normalized vectors by squared mag for plotting,
     need to check that this is OK
     '''
-    cla()
-    R  = abs(mean(vectors))/mean(abs(vectors))
-    S  = sqrt(-2*log(R))
-    mu = angle(mean(vectors))
-    vectors = vectors/mean(abs(vectors)**2)**.5
+    plt.cla()
+    R  = np.abs(np.mean(vectors))/np.mean(np.abs(vectors))
+    S  = np.sqrt(-2*np.log(R))
+    mu = np.angle(np.mean(vectors))
+    vectors = vectors/np.mean(np.abs(vectors)**2)**.5
     vector_summary_plot_subroutine(mu,S,vectors)
 
 
